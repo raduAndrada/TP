4e79fc8e8aa9053c84b419fd48008e7b827745f5
raduAndrada <radu.andrada8@gmail.com>
2017-12-09 10:23:55 +0200

    schimbari recipe

--- src/laborator7/XSlFoToPDF.java
+++ src/laborator7/XSlFoToPDF.java
@@ -4,15 +4,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
-import java.util.stream.Stream;
 
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.transform.sax.SAXResult;
 import javax.xml.transform.stream.StreamSource;
 
@@ -22,7 +19,6 @@ import org.apache.fop.apps.Fop;
 import org.apache.fop.apps.FopFactory;
 import org.apache.fop.apps.FormattingResults;
 import org.apache.fop.apps.MimeConstants;
-import org.apache.fop.render.afp.*;
 
 public class XSlFoToPDF {
 
@@ -30,20 +26,20 @@ public class XSlFoToPDF {
 	private static final String PDF_OUTPUT_DESTINATION = "src/laborator7/recipe.pdf";
 
 	public static void main(String args[]) {
-		FopFactory ff = FopFactory.newInstance();
-		FOUserAgent foUa = ff.newFOUserAgent();
-		Source src = new StreamSource(STREAM_SOURCE_LOCATION);
+		final FopFactory ff = FopFactory.newInstance();
+		final FOUserAgent foUa = ff.newFOUserAgent();
+		final Source src = new StreamSource(STREAM_SOURCE_LOCATION);
 		try {
-			OutputStream out = new FileOutputStream(new File(PDF_OUTPUT_DESTINATION));
-			Fop fop = ff.newFop(MimeConstants.MIME_PDF, foUa, out);
-			TransformerFactory trFact = TransformerFactory.newInstance();
+			final OutputStream out = new FileOutputStream(new File(PDF_OUTPUT_DESTINATION));
+			final Fop fop = ff.newFop(MimeConstants.MIME_PDF, foUa, out);
+			final TransformerFactory trFact = TransformerFactory.newInstance();
 
-			Transformer tr = trFact.newTransformer();
+			final Transformer tr = trFact.newTransformer();
 
-			Result res = new SAXResult(fop.getDefaultHandler());
+			final Result res = new SAXResult(fop.getDefaultHandler());
 			tr.transform(src, res);
-			FormattingResults foRes = fop.getResults();
-			System.out.print("Generated: - "+ foRes.getPageCount() + " pages");
+			final FormattingResults foRes = fop.getResults();
+			System.out.print("Generated: - " + foRes.getPageCount() + " pages");
 		} catch (FileNotFoundException | FOPException | TransformerException e) {
 			e.printStackTrace();
 		}
